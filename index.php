944e8aa31ddf28fd3e89f170d2f5db01b4cc6016
Jinank Jain <ug201210017@iitj.ac.in>
2014-10-07 17:14:25 +0530

    Bootstrap set with initial page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+</head>
+<body>
+	<div class="container-fluid">
+	<div class="row">
+		<nav class="navbar navbar-inverse" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="http://spc.iitj.ac.in/">Student Placement Cell, IIT Jodhpur</a>
+		    </div>
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="col-xs-4"></div>
+			<div class="col-xs-4">
+				<h1 align="center">Login</h1><br><br>
+				<form class="form-horizontal" role="form">
+				  <div class="form-group">
+				    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+				    <div class="col-sm-10">
+				      <input type="email" class="form-control" id="inputEmail3" placeholder="Username">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+				    <div class="col-sm-10">
+				      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" class="btn btn-primary">Sign in</button>
+				    </div>
+				  </div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src=	"bootstrap/js/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
